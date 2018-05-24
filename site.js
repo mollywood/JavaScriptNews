27e6ba035ffe08c84b816f3a79766cb0ca7ce280
Molly Wood <mollywood@Interzone.local>
2018-05-24 17:08:30 -0500

    functioning

--- site.js
+++ site.js
@@ -1,1 +1,20 @@
 let newsDiv = document.querySelector('#newsDiv')
+addEventListener('load', function(){
+  populateNews(news)
+})
+
+function populateNews(news) {
+  newsDiv.innerHTML = ''
+  let newsToDisplay = news['articles']
+  for(let index = 0; index < newsToDisplay; index++) {
+
+    let newsItem = `${newsToDisplay[index].title}</h4>
+    <h5>${newsToDisplay[index].author}</h5>
+    <p>${newsToDisplay[index].description}</p>
+    <a href='${newsToDisplay[index].url}'>Read More</a>
+    <img src='${newsToDisplay[index].urlToImage}' />
+    <h6>${newsToDisplay[index].publishedAt}</h6>`
+
+    newsDiv.innerHTML += newsItem
+  }
+}
