39396d39081c3ec49bce5c4df6122dbebd5502cb
Molly Wood <mollywood@Interzone.local>
2018-05-24 22:09:04 -0500

    updating layout

--- site.js
+++ site.js
@@ -9,10 +9,10 @@ function populateNews(news) {
   newsToDisplay.forEach(function(news) {
 
     let newsItem = `<h4>${news.title}</h4>
+    <img src='${news.urlToImage}' />
     <h5>${news.author}</h5>
     <p>${news.description}</p>
     <a href='${news.url}'>Read More</a>
-    <img src='${news.urlToImage}' />
     <h6>${news.publishedAt}</h6>`
 
     newsDiv.innerHTML += newsItem
